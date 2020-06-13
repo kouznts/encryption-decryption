d955560082ae55e7136c20199fbfee732b8e6408
Andrey <kouznts@ya.ru>
2020-06-13 19:08:03 +0400

    create web crawler class

--- web-crawler/src/com/company/ApplicationRunner.java
+++ web-crawler/src/com/company/ApplicationRunner.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        new WebCrawlerWindow();
+        new WebCrawler();
     }
 }
 
---
+++ web-crawler/src/com/company/WebCrawler.java
@@ -0,0 +1,90 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class WebCrawler extends JFrame {
+    JPanel panel;
+    JTextField tfUrl;
+    JButton btnRun;
+    JTextArea taHtml;
+
+    public WebCrawler() {
+        setTfUrl();
+        setTaHtml();
+        setBtnRun();
+
+        setPanel();
+        add(panel);
+
+        setWebCrawlerFrame();
+    }
+
+    private void setTfUrl() {
+        tfUrl = new JTextField();
+        tfUrl.setName("UrlTextField");
+        tfUrl.setPreferredSize(new Dimension(100, 30));
+        tfUrl.setVisible(true);
+    }
+
+    private void setTaHtml() {
+        taHtml = new JTextArea("HTML code?");
+        taHtml.setName("HtmlTextArea");
+        taHtml.setPreferredSize(new Dimension(100, 100));
+        taHtml.setEnabled(false);
+        taHtml.setEditable(false);
+        taHtml.setHighlighter(null);
+        taHtml.setVisible(true);
+    }
+
+    private void setBtnRun() {
+        btnRun = new JButton();
+        btnRun.setName("RunButton");
+        btnRun.setPreferredSize(new Dimension(100, 100));
+        btnRun.setVisible(true);
+
+        btnRun.addActionListener(ev ->
+                taHtml.setText(
+                        downloadWebpage(tfUrl.getText())));
+    }
+
+    private void setPanel() {
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        panel.add(tfUrl);
+        panel.add(taHtml);
+        panel.add(btnRun);
+    }
+
+    private void setWebCrawlerFrame() {
+        setTitle("WebCrawler");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(500, 500);
+        setPreferredSize(new Dimension(500, 500));
+        setVisible(true);
+    }
+
+
+    private String downloadWebpage(final String url) {
+        String siteText = "";
+
+        try (InputStream inputStream =
+                     new BufferedInputStream(
+                             new URL(url).openStream())) {
+
+            siteText =
+                    new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+
+        return siteText;
+    }
+}
