7732ae4a6725a1b4e66544b4df29a12ea737d664
Andrey <kouznts@ya.ru>
2020-06-23 10:44:02 +0400

    impl parse webpage urls method

--- web-crawler/src/com/company/WebCrawling.java
+++ web-crawler/src/com/company/WebCrawling.java
@@ -40,7 +40,7 @@ public class WebCrawling {
         return webpageTitle;
     }
 
-    public static List<String> parseWebpageLinks(String webpageHtmlCode) {
+    public static List<String> parseWebpageUrls(final String webpageUrl) {
         List<String> urls = new ArrayList<String>();
 
         Pattern patternLinkTag = Pattern.compile("(?i)(<a.*?href=[\"'])(.*?)([\"'].*?>)(.*?)(</a>)");
@@ -48,12 +48,36 @@ public class WebCrawling {
                 parseWebpageHtmlCode(webpageUrl)
         );
 
-        String parsedLink;
-        while (matcherWebpage.find()) {
-            parsedLink = matcherWebpage.group(2);
-            urls.add(parsedLink);
+        String webpageUrlProtocol = webpageUrl.substring(0, webpageUrl.indexOf("//"));
+
+        Matcher matcherWebpageUrlBase = Pattern.compile("^.+/").matcher(webpageUrl);
+        String webpageUrlBase = "";
+        if (matcherWebpageUrlBase.find()) {
+            webpageUrlBase = matcherWebpageUrlBase.group();
+        }
+
+        StringBuilder parsedUrl;
+        while (matcherWebpageHtmlCode.find()) {
+            parsedUrl = new StringBuilder().append(
+                    matcherWebpageHtmlCode.group(2)
+            );
+
+            if (parsedUrl.toString().startsWith("#")) {
+                continue;
+            } else if (!parsedUrl.toString().startsWith("http")) {
+                if (parsedUrl.toString().startsWith("//")) {
+                    parsedUrl.insert(0, webpageUrlProtocol);
+                } else if (parsedUrl.toString().startsWith("/")) {
+                    parsedUrl.insert(0, webpageUrl);
+                } else {
+                    parsedUrl.insert(0, webpageUrlBase);
+        }
+            }
+
+            urls.add(parsedUrl.toString());
         }
 
         return urls;
     }
+
 }
