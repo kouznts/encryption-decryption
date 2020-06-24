32ca69c86ea376f14e7d4362ce1f15de40ecb1a3
Andrey <kouznts@ya.ru>
2020-06-24 09:13:22 +0400

    url connection as human

--- web-crawler/src/com/company/WebCrawling.java
+++ web-crawler/src/com/company/WebCrawling.java
@@ -19,6 +19,8 @@ public class WebCrawling {
 
         try {
             URLConnection urlConnection = new URL(webpageUrl).openConnection();
+            urlConnection.setRequestProperty("User-Agent",
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0");
 
             int responseCode = ((HttpURLConnection) urlConnection).getResponseCode();
             if (responseCode == 404
